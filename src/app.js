b77896489e3c7ec9f3b5a0c8fc97024c14170102
SketchitGuessit <vpednekar1235@gmail.com>
2023-12-24 21:06:44 +0530

    hbs added

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 const express = require('express');
-const path = require('path')
-const app = express()
+const path = require('path');
+const app = express();
+const hbs = require('hbs');
 const port = process.env.PORT || 8000;
 
-const public_path = path.join(__dirname, "../public")
-app.use(express.static(public_path));
+//providing static path 
+// const public_path = path.join(__dirname, "../public")
+// app.use(express.static(public_path));
+
+//using dynamic path
+const template_path = path.join(__dirname , "../tempaltes/views")
+app.set('view engine' , 'hbs');
+app.set('views' , template_path);
 
 app.get("/" , (req , res) =>{
-    res.send("Welcome to vaibhav's  GRAND world")
+    res.render("index")
 })
 
-app.get("/about" , (req , res) =>{
-    res.send("welocme to vaibhav 's about world")
+app.get("/login" , (req , res) =>{
+    res.render('login')
 })
 app.get("/contact" , (req , res) =>{
     res.send("Welcome to vaibhav's contact  world")
